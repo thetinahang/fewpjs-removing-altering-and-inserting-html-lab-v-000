34c653df814b97b961cc2caee4072ef888588645
Tina Hang <mstinahang@gmail.com>
2020-01-04 20:08:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let element = document.querySelector('#main');
+element.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "YOUR-NAME is the champion";
